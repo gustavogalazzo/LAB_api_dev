2157bf036d6cd2864eb87f3bd6eae32db8f9350b
Dan Parker <admin@MacBook-de-Dan.local>
2024-10-18 19:23:23 -0300

    api atualizada

--- api/controller/faculdade_controller.py
+++ api/controller/faculdade_controller.py
@@ -1,20 +1,22 @@
 # Controllers: onde fica a lógica dos endpoints
 from flask import Blueprint, jsonify, request
-from models.faculdade import professores, turmas, alunos
+from api.models.faculdade import professores, turmas, alunos
 
-faculdade_bp = Blueprint('faculdade', __name__)
+professores_bp = Blueprint('professores', __name__)
+turmas_bp = Blueprint('turmas', __name__)
+alunos_bp = Blueprint('alunos', __name__)
 
-@faculdade_bp.route('/', methods=['GET'])
+@professores_bp.route('/', methods=['GET'])
 def api():
     return 'A API está rodando'
 
 # Rota para listar todos os professores
-@faculdade_bp.route('/professores', methods=['GET'])
+@professores_bp.route('/professores', methods=['GET'])
 def listar_professores():
     return jsonify({'professores': professores})
 
 # Rota para listar um professor específico
-@faculdade_bp.route('/professor/<int:prof_id>', methods=['GET'])
+@professores_bp.route('/professor/<int:prof_id>', methods=['GET'])
 def professor_id(prof_id):
     for professor in professores:
         if professor['id'] == prof_id:
@@ -22,14 +24,14 @@ def professor_id(prof_id):
     return jsonify({'mensagem': 'Professor não encontrado'}), 404
 
 # Cadastrar professor
-@faculdade_bp.route('/professor', methods=['POST'])
+@professores_bp.route('/professor', methods=['POST'])
 def cadastrar_prof():
     prof = request.json
     professores.append(prof)
     return jsonify({'Professor cadastrado com sucesso!': prof}), 201
 
 # Atualizar professor
-@faculdade_bp.route('/professor/<int:prof_id>', methods=['PUT'])
+@professores_bp.route('/professor/<int:prof_id>', methods=['PUT'])
 def update_user(prof_id):
     for professor in professores:
         if professor['id'] == prof_id:
@@ -42,7 +44,7 @@ def update_user(prof_id):
     return jsonify({'mensagem': 'Usuário não encontrado'}), 404
 
 # Deletar professor
-@faculdade_bp.route('/professor/<int:prof_id>', methods=['DELETE'])
+@professores_bp.route('/professor/<int:prof_id>', methods=['DELETE'])
 def delete_user(prof_id):
     for professor in professores:
         if professor['id'] == prof_id:
@@ -51,12 +53,12 @@ def delete_user(prof_id):
     return jsonify({'mensagem': 'Professor não encontrado'}), 404
 
 # Rota para listar todas as turmas
-@faculdade_bp.route('/turmas', methods=['GET'])
+@turmas_bp.route('/turmas', methods=['GET'])
 def listar_turmas():
     return jsonify({'turmas': turmas})
 
 # Rota para listar uma turma específica com o nome do professor
-@faculdade_bp.route('/turmas/<int:id>', methods=['GET'])
+@turmas_bp.route('/turmas/<int:id>', methods=['GET'])
 def turma_por_id(id):
     for turma in turmas:
         if turma['id'] == id:
@@ -65,8 +67,10 @@ def turma_por_id(id):
                     return jsonify({'turma': turma,'professor': {'nome': professor['nome']}})
     return jsonify({'erro': 'Turma não encontrada'}), 404
 
+# Rota para listar todos os professores
+
 # Rota para listar um aluno específico
-@faculdade_bp.route('/aluno/<int:aluno_id>', methods=['GET'])
+@alunos_bp.route('/aluno/<int:aluno_id>', methods=['GET'])
 def aluno_por_id(aluno_id):
     for aluno in alunos:
         if aluno['id'] == aluno_id:
--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 # app.py vai apenas inicializar a aplicação e importar os controladores.
 from flask import Flask
-from controller.faculdade_controller import faculdade_bp
+from api.controller.faculdade_controller import professores_bp, turmas_bp, alunos_bp
 
 app = Flask(__name__)
 
 # Registrar os blueprints (rotas organizadas)
-app.register_blueprint(faculdade_bp)
+app.register_blueprint(professores_bp)
+app.register_blueprint(turmas_bp)
+app.register_blueprint(alunos_bp)
 
 if __name__ == '__main__':
     app.run(debug=True)
